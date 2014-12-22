7ebb2126656fd4b19f703434d24d082b9e9b40b3
majodev <contact@majodev.com>
2014-12-22 21:26:43 +0100

    loading styling and layout

--- client/app/fonts/fonts.controller.js
+++ client/app/fonts/fonts.controller.js
@@ -4,20 +4,25 @@ angular.module('googleWebfontsHelperApp')
   .controller('FontsCtrl', function($scope, $http) {
 
     $scope.fonts = [];
+    $scope.busy = true;
 
     $scope.fontsPromise = $http.get('/api/fonts').success(function(fonts) {
       $scope.fonts = fonts;
+      $scope.busy = false;
     });
 
   })
 
   .controller('FontsItemCtrl', function($scope, $stateParams, $http) {
-
     $scope.fontID = $stateParams.id;
     $scope.fontItem = {};
 
+    $scope.busy = true;
+
     $scope.fontItemPromise = $http.get('/api/fonts/' + $stateParams.id).success(function(fontItem) {
       $scope.fontItem = fontItem;
+
+      $scope.busy = false;
     });
 
   });
