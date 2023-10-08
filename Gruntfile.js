d167026d3d481b8af6ffb057af93e1da4bfa2587
Mario Ranftl <code@mranftl.com>
2023-10-08 16:46:04 +0200

    wire disable exclude bootstrap

--- Gruntfile.js
+++ Gruntfile.js
@@ -153,7 +153,7 @@ module.exports = function (grunt) {
       target: {
         src: '<%= yeoman.client %>/index.html',
         ignorePath: '<%= yeoman.client %>/',
-        exclude: [/bootstrap-sass-official/, /bootstrap.js/, '/json3/', '/es5-shim/', /bootstrap.css/, /font-awesome.css/]
+        exclude: [/bootstrap-sass-official/, '/json3/', '/es5-shim/', /bootstrap.css/, /font-awesome.css/]
       }
     },
 
